d5e5b8ffe09e8ac6d71effad37001234e3734dc7
Greg Lomax <greg_lomax@homedepot.com>
2016-11-02 08:53:58 -0400

    App files

---
+++ app/services/github.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+    private username:string;
+    private client_id = '4cc84abe43043bcea420';
+    private client_secret = '40c253398788cb65994aca61720195f2e3ec7482';
+
+    constructor(private _http: Http){
+        console.log('Github Service Ready...');
+        this.username = 'greglomax';
+    }
+    getUser(){
+        return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+        .map(res => res.json());
+    }
+    getRepos(){
+        return this._http.get('http://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+        .map(res => res.json());
+    }
+    updateUser(username:string){
+        this.username=username;
+    }
+}
